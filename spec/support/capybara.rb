fe21bb67ec894c26ebdcea4dec6abba4614d01c0
carlost <514078+carlost@users.noreply.github.com>
2025-06-21 13:43:57 -0400

    no JS driver support for now

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,10 +3,4 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
-  
-  # Configure JavaScript driver for specs that need browser automation
-  # Usage: it "test name", js: true do
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
 end
