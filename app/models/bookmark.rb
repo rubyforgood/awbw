3b40cb30add52eb6b8bed1217c52ae831d7cbdb3
maebeale <maebeale@users.noreply.github.com>
2025-09-12 12:20:25 -0400

    Convert 'FIELD' implementation and change map to pluck - consolidate multiple queries to one - change  to  for performance improvements

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,28 +5,28 @@ class Bookmark < ApplicationRecord
 
   def self.sort_by_windows_type(bookmarks, windows_type_id)
     if windows_type_id == "3"
-      workshops = Workshop.where(id: bookmarks.map{|b| b.bookmarkable_id}).order(windows_type_id: :desc)
+      workshops = Workshop.where(id: bookmarks.pluck{|b| b.bookmarkable_id}).order(windows_type_id: :desc)
     elsif windows_type_id == "1"
-      workshops = Workshop.where(id: bookmarks.map{|b| b.bookmarkable_id}).order(windows_type_id: :asc)
+      workshops = Workshop.where(id: bookmarks.pluck{|b| b.bookmarkable_id}).order(windows_type_id: :asc)
     end
 
-    workshops_ids = workshops.map{|w| w.id}
-    bookmarks = bookmarks.where(bookmarkable_id: workshops_ids).order("FIELD(bookmarkable_id, #{workshops_ids.join(',')})")
+    workshops_ids = workshops.pluck{|w| w.id}
+    bookmarks = bookmarks.where(bookmarkable_id: workshops_ids).order(:windows_type_id)
   end
 
   def self.search(params, user)
     bookmarks = user.bookmarks
 
     if params[:type].nil? || params[:type].empty?
-      workshops = Workshop.where(id: bookmarks.map{|b| b.bookmarkable_id}).order(title: :asc)
-      workshops_ids = workshops.map{|w| w.id}
-      bookmarks = bookmarks.where(bookmarkable_id: workshops_ids).order("FIELD(bookmarkable_id, #{workshops_ids.join(',')})")
+      bookmarks = bookmarks.where(bookmarkable_type: "Workshop")
+                           .joins("INNER JOIN workshops ON bookmarks.bookmarkable_id = workshops.id")
+                           .order("workshops.title")
     end
 
     if params[:type] == "led"
-      workshops = Workshop.where(id: bookmarks.map{|b| b.bookmarkable_id}).order(led_count: :desc)
-      workshops_ids = workshops.map{|w| w.id}
-      bookmarks = bookmarks.where(bookmarkable_id: workshops_ids).order("FIELD(bookmarkable_id, #{workshops_ids.join(',')})")
+      bookmarks = bookmarks.where(bookmarkable_type: "Workshop")
+                           .joins("INNER JOIN workshops ON bookmarks.bookmarkable_id = workshops.id")
+                           .order("workshops.led_count")
     end
 
     if params[:type] == "created"
