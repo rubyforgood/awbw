bf741772d20ad1b0599f53c7d2770a409171eed2
John Paul Ashenfelter <john@ashenfelter.com>
2025-09-12 06:19:24 -0400

    Simplify the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,15 @@
-# Resolve the correct DB credentials
-config = ActiveRecord::Base.configurations[Rails.env]
+sql_file = Rails.root.join('db', 'awbw_dml_only.sql')
+puts "Loading SQL dump from #{sql_file}..."
+command = "rails dbconsole < #{sql_file}"
+system(command) || raise("Failed to load SQL dump into #{database}")
 
-# Extract credentials
-adapter = config[:adapter]
-database = config[:database]
+admin_password = Devise::Encryptor.digest(Admin, 'password')
+Admin.update_all(encrypted_password: admin_password)
 
-# Only run if MySQL is the DB
-if adapter.include?('trilogy')
-  sql_file = Rails.root.join('db', 'awbw_dml_only.sql')
-
-  puts "Loading SQL dump from #{sql_file}..."
-
-  # Run the command
-  command = "rails dbconsole < #{sql_file}"
-  system(command) || raise("Failed to load SQL dump into #{database}")
-  
-  puts "SUCCESS! SQL dump loaded successfully."
-else
-  puts "Skipping SQL dump: not using MySQL (adapter = #{adapter})"
+user_password = Devise::Encryptor.digest(User, 'password')
+User.in_batches do |batch|
+  batch.update_all(encrypted_password: user_password)
 end
 
-# wrapping in a tx for now
-ActiveRecord::Base.transaction do
-  admin_password = Devise::Encryptor.digest(Admin, 'password')
-  Admin.update_all(encrypted_password: admin_password)
-
-  user_password = Devise::Encryptor.digest(User, 'password')
-  User.in_batches do |batch|
-    batch.update_all(encrypted_password: user_password)
-  end
-
-  Admin.create!(first_name: "Amy", last_name: "Admin", email: "amy.admin@example.com", password: "password")
-  User.create!(first_name: "Umberto", last_name: "User", email: "umberto.user@example.com", password: "password")
-end
+Admin.create!(first_name: "Amy", last_name: "Admin", email: "amy.admin@example.com", password: "password")
+User.create!(first_name: "Umberto", last_name: "User", email: "umberto.user@example.com", password: "password")
