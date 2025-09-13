77786c5838404d10cc642a30aa6f0564fa0ca82a
elasticspoon <mail@yuribocharov.dev>
2025-09-13 15:51:33 -0400

    fix: event spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,15 +7,5 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { "password" }
     password_confirmation { "password" }
-
-    trait :with_perms do
-      after(:create) do |user|
-        # Create all permission types and associate them with the user
-        [:adult, :children, :combined].each do |perm_trait|
-          permission = create(:permission, perm_trait)
-          user.permissions << permission
-        end
-      end
-    end
   end
 end
