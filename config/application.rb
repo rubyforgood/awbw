eecaef4bb2ff167fd055696f176dc832c727a072
John Paul Ashenfelter <john@ashenfelter.com>
2025-09-13 10:46:14 -0400

    Handle missing/problem images with Rails 7.1 stricter sprockets assets

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,5 @@ module Awbw
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.paperclip_defaults = { storage: :fog, fog_credentials: { provider: "Local", local_root: "#{Rails.root}/public"}, fog_directory: "", fog_host: "localhost"}
-    # config.paperclip_defaults = { storage: :filesystem }
-    # Paperclip::Attachment.default_options[:storage] = :fog
-    # Paperclip::Attachment.default_options[:fog_credentials] = { provider: "Local", local_root: "#{Rails.root}/public"}
-    # Paperclip::Attachment.default_options[:fog_directory] = ""
-    # Paperclip::Attachment.default_options[:fog_host] = "http://localhost:3000"
   end
 end
