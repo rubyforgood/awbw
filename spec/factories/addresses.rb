c07277e8adb89ee979f9336f4f347d824be9f3cf
Dalma Boros <dalma.boros@gmail.com>
2025-09-13 14:57:10 -0400

    Update factories to use Faker

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -2,16 +2,16 @@ FactoryBot.define do
   factory :address do
     association :organization
     
-    street { "123 Main Street" }
-    city { "Los Angeles" }
-    state { "CA" }
-    zip { "90210" }
-    
-    country { "United States" }
-    locality { "LA City" }
-    county { "Los Angeles" }
-    la_city_council_district { 1 }
-    la_supervisorial_district { 1 }
-    la_service_planning_area { 1 }
+    street { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip { Faker::Address.zip_code }
+
+    country { Faker::Address.country}
+    locality { ["LA City", "LA County", "Southern CA", "Northern CA", "Central CA", "Orange County", "Outside CA", "Outside USA"].sample }
+    county { Faker::Address.state }
+    la_city_council_district { Faker::Number.between(from: 1, to: 15) }
+    la_supervisorial_district { Faker::Number.between(from: 1, to: 5) }
+    la_service_planning_area { Faker::Number.between(from: 1, to: 8) }
   end
 end
