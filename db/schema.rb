866d5d305615fb43ed7693c3f3ba236f3fba61c5
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-09-16 12:19:46 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,11 @@
 
 ActiveRecord::Schema[8.1].define(version: 2025_10_25_164126) do
   create_table "active_storage_attachments", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -767,9 +767,9 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_25_164126) do
     t.index ["year", "month"], name: "index_workshops_on_year_and_month"
   end
 
-  add_foreign_key "addresses", "organizations"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "organizations"
   add_foreign_key "age_ranges", "windows_types"
   add_foreign_key "bookmark_annotations", "bookmarks"
   add_foreign_key "bookmarks", "users"
