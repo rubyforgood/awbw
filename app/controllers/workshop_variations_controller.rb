0639d8b22ced7916f320e44323d7718cc2cc5946
maebeale <maebeale@users.noreply.github.com>
2025-09-20 13:00:30 -0400

    Add CRUD views and actions for workshop_variations

--- app/controllers/workshop_variations_controller.rb
+++ app/controllers/workshop_variations_controller.rb
@@ -1,4 +1,27 @@
 class WorkshopVariationsController < ApplicationController
+
+  def index
+    @workshop_variations = WorkshopVariation.joins(:workshop).
+      where(workshops: { inactive: false }).
+      order('workshops.title, workshop_variations.name').
+      paginate(page: params[:page], per_page: 25)
+  end
+
+  def new
+    @workshop_variation = WorkshopVariation.new
+    @workshops = Workshop.published
+  end
+
+  def create
+    @workshop_variation = WorkshopVariation.new(workshop_variation_params)
+    if @workshop_variation.save
+      flash[:alert] = 'Workshop Variation has been created.'
+      redirect_to workshop_variations_path
+    else
+      render :new
+    end
+  end
+
   def show
     @workshop_variation = WorkshopVariation.find(params[:id]).decorate
     @workshop = @workshop_variation.workshop.decorate
@@ -8,4 +31,14 @@ class WorkshopVariationsController < ApplicationController
     @workshop_variations = @workshop.workshop_variations
     @sectors = @workshop.sectors
   end
+
+  def edit
+    @workshop_variation = WorkshopVariation.find(params[:id])
+    @workshops = Workshop.published
+  end
+
+  def workshop_variation_params
+    [:name, :code, :inactive, :ordering, :workshop_id]
+  end
+
 end
