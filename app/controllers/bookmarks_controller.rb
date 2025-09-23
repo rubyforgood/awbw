d74b6483c02ac74650d06095867cd8d4bcc7dcea
maebeale <maebeale@users.noreply.github.com>
2025-09-23 11:26:36 -0400

    Default search results to 25 instead of 3

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,8 @@ class BookmarksController < ApplicationController
   before_action :set_breadcrumb
 
   def index
-    @bookmarks = Bookmark.search(params, current_user).paginate(page: params[:page], per_page: 3)
+    @bookmarks = Bookmark.search(params, current_user).paginate(page: params[:page], per_page: 25)
+
     load_sortable_fields
     respond_to do |format|
       format.html
