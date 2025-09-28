24b46305020031d073fab0cad09d387e220e53c1
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-09-28 07:32:16 -0400

    create stimulus generator for vite file path

---
+++ lib/generators/vite_stimulus/vite_stimulus_generator.rb
@@ -0,0 +1,39 @@
+require "rails/generators/named_base"
+
+class ViteStimulusGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("templates", __dir__)
+
+  def copy_view_files
+    @attribute = stimulus_attribute_value(controller_name)
+    template "controller.js", "app/frontend/javascript/controllers/#{controller_name}_controller.js"
+
+    update_index_js
+  end
+
+  private
+
+  def controller_name
+    name.underscore.gsub(/_controller$/, "")
+  end
+
+  def stimulus_attribute_value(controller_name)
+    controller_name.gsub("/", "--").tr("_", "-")
+  end
+
+  def update_index_js
+    controllers_path = Rails.root.join("app/frontend/javascript/controllers")
+    index_file = controllers_path.join("index.js")
+
+    import_line = "import #{controller_name.camelize}Controller from \"./#{controller_name}_controller\""
+    register_line = "application.register(\"#{controller_name}\", #{controller_name.camelize}Controller)"
+
+    # Avoid duplicate entries
+    content = File.read(index_file)
+    unless content.include?(import_line)
+      File.open(index_file, "a") do |f|
+        f.puts "\n#{import_line}"
+        f.puts register_line
+      end
+    end
+  end
+end
