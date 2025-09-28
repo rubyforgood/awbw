c7a9781b0b38665075c591dd7fffbd9c176bdfaa
maebeale <maebeale@users.noreply.github.com>
2025-09-28 00:40:11 -0400

    WIP: admin dashboard - WIP list of what admins will need access to - WIP idea of using hard-coded icons (we don't have latest font-awesome yet) - WIP putting root_path for CRUD that doesn't exist yet

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -20,6 +20,45 @@ class DashboardController < ApplicationController
     @recent_activity = current_user.recent_activity
   end
 
+  def admin
+    if current_user.super_user?
+      @user_content_cards = [
+
+        { title: "Bookmarks tally", path: root_path, icon: "🔖" },
+        { title: "Quotes", path: root_path, icon: "💬" },
+        { title: "Stories", path: root_path, icon: "🗣️" },
+        { title: "Annual Reports", path: root_path, icon: "📊" },
+        { title: "Workshop Logs", path: workshop_logs_path, icon: "📝" },
+        { title: "Workshops", path: workshops_path, icon: "🎨" },
+        { title: "Workshop Ideas", path: root_path, icon: "💡" },
+        { title: "Workshop Variations", path: workshop_variations_path, icon: "🔀" },
+      ]
+
+      @system_cards = [
+        { title: "Banners", path: root_path, icon: "📣" },
+        { title: "Events", path: events_path, icon: "📆" },
+        { title: "FAQs", path: faqs_path, icon: "❔" },
+        { title: "Forms", path: root_path, icon: "📋" },
+        { title: "Organizations", path: root_path, icon: "🏫" },
+        { title: "Resources", path: resources_path, icon: "📚" },
+        { title: "Users", path: users_path, icon: "👥" },
+
+      ]
+
+      @reference_cards = [
+
+        # { title: "Age ranges", path: root_path, icon: "👶" },
+        { title: "Categories", path: root_path, icon: "🗂️" },
+        { title: "Sectors", path: root_path, icon: "🏭" },
+        # { title: "WindowsTypes", path: root_path, icon: "🪟" },
+        # { title: "FormFields", path: root_path, icon: "✏️" },
+        # { title: "FormAnswerOptions", path: root_path, icon: "🗳️" },
+      ]
+    else
+      redirect_to root_path, alert: 'You do not have permission.'
+    end
+  end
+
   def recent_activity
     @recent_activity = current_user.recent_activity(20)
   end
