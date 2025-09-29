e18bf81b4e5aa0cb98c47c5655fa02bccc49086f
maebeale <maebeale@users.noreply.github.com>
2025-09-29 18:28:19 +0000

    deploy: 9292da1cfe88003065c3b6b668ffc1b673900a71

--- badges/main/spec/requests/bookmarks_spec.rb
+++ badges/main/spec/requests/bookmarks_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Bookmarks", type: :request do
 		sign_in user
 	end
 
-	describe "DELETE /bookmarks/:id" do
+	describe "DELETE /bookmarks/:id from=index" do
 		it "destroys a bookmark and redirects to index if from=index" do
 			expect {
 				delete bookmark_path(bookmark), params: { from: "index" }
@@ -32,6 +32,50 @@ RSpec.describe "Bookmarks", type: :request do
 
 			expect(Bookmark.exists?(bookmark.id)).to be_falsey
 			expect(response).to redirect_to(workshop_path(workshop))
+		end
+	end
+
+	describe "DELETE /bookmarks/:id from=workshops_index" do
+		before { bookmark } # ensure bookmark exists
+		let(:workshop) { create(:workshop) }
+
+		it "deletes the bookmark and redirects to workshops index with query params and anchor" do
+			delete bookmark_path(bookmark), params: { from: "workshops_index", title: "Art", query: "painting" }
+
+			# Should delete the bookmark
+			expect(Bookmark.exists?(bookmark.id)).to be false
+
+			# Parse the redirect URL
+			uri = URI.parse(response.location)
+			expect(uri.path).to eq(workshops_path)
+			expect(uri.query).to include("title=Art")
+			expect(uri.query).to include("query=painting")
+			# expect(uri.fragment).to eq("workshop-#{workshop.id}-anchor") can't test this bc Rack::Test doesn't support fragments
+		end
+	end
+
+	describe "POST /bookmarks from=workshops_index" do
+		before { bookmark } # ensure bookmark exists
+		let(:workshop) { create(:workshop) }
+
+		it "creates a bookmark and redirects to workshops index with query params and anchor" do
+			post bookmarks_path, params: {
+				bookmark: { bookmarkable_id: workshop.id, bookmarkable_type: "Workshop" },
+				from: "workshops_index",
+				title: "Art",
+				query: "painting"
+			}
+
+			# Should create the bookmark
+			expect(Bookmark.exists?(user: user, bookmarkable: workshop)).to be true
+
+			# Parse the redirect URL
+			uri = URI.parse(response.location)
+
+			expect(uri.path).to eq(workshops_path)
+			expect(uri.query).to include("title=Art")
+			expect(uri.query).to include("query=painting")
+			# expect(uri.fragment).to eq("workshop-#{workshop.id}-anchor") can't test this bc Rack::Test doesn't support fragments
 		end
 	end
 end
