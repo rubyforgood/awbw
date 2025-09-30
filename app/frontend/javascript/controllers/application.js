a561d89e47aecda311587beb72cf4d36f2a0107d
maebeale <maebeale@users.noreply.github.com>
2025-09-29 23:12:57 -0400

    Use stimulus to toggle between english and spanish descriptions of workshops

--- app/frontend/javascript/controllers/application.js
+++ app/frontend/javascript/controllers/application.js
@@ -1,9 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import TabsController from "./tabs_controller"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
-application.debug = true 
-window.Stimulus   = application
+// Register your controller
+application.register("tabs", TabsController)
+
+// Optional: enable debug mode and console access
+application.debug = true
+window.Stimulus = application
 
 export { application }
