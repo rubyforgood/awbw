ca8c0232745532b900ba9113b97ed0791210c425
maebeale <maebeale@users.noreply.github.com>
2025-09-30 13:55:30 -0400

    Update ApplicationController - Protect from forgery with exception (was only with null_session for api calls) - Remove authenticate_api_user (it's in the api controller, and no longer needed for RailsAdmin) - Remove authenticate_user custom behavior as that legacy migration was done in 2016

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
-  before_action :authenticate_api_user!
-  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!  # ensures only logged-in users can access pages
 
   private
-
-  def authenticate_api_user!
-    return unless current_api_user   # do nothing if no API user
-
-    sign_in(current_api_user)
-    flash[:notice] ||= 'You have successfully logged in.' # use :info
-  end
-
-  def authenticate_user!
-    user = User.find_by(email: params[:user][:email]) if params[:user]
-    if user && user.legacy
-      flash[:notice] = 'We have migrated our data to a new system.  '\
-                       'Please click the link below to reset your password.'
-    end
-
-    super
-  end
 
   # IMPERSONATE USER
   def current_user
@@ -39,22 +21,5 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     new_user_session_path
-  end
-
-  def current_api_user
-    if http_auth_header_content
-      @current_user ||= User.find_by(id: AuthenticationToken.new(http_auth_header_content).user_id)
-    elsif current_user
-      @current_user
-    end
-  end
-
-  def http_auth_header_content
-    if request.headers['Authorization'].present?
-      @http_auth_header_content ||=
-        request.headers['Authorization'].split(' ').last
-    else
-      @http_auth_header_content ||= params['Authorization']
-    end
   end
 end
