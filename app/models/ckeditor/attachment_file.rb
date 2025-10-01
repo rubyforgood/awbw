736376889ff0ba6720aa2bc791adce2dcab2abcb
maebeale <maebeale@users.noreply.github.com>
2025-09-30 23:01:01 -0400

    Quick and dirty commenting/inheritance changes

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,13 +1,13 @@
-class Ckeditor::AttachmentFile < Ckeditor::Asset
-  has_attached_file :data,
-                    :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => "/ckeditor_assets/attachments/:id/:filename"
-
-  validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 100.megabytes
-  do_not_validate_attachment_file_type :data
+class Ckeditor::AttachmentFile < ApplicationRecord # Ckeditor::Asset
+  # has_attached_file :data,
+  #                   :url => "/ckeditor_assets/attachments/:id/:filename",
+  #                   :path => "/ckeditor_assets/attachments/:id/:filename"
+  #
+  # validates_attachment_presence :data
+  # validates_attachment_size :data, :less_than => 100.megabytes
+  # do_not_validate_attachment_file_type :data
 
   def url_thumb
-    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+    @url_thumb ||= nil # Ckeditor::Utils.filethumb(filename)
   end
 end
