f8308a2d810f284b89ac991cf7bdac9ca3c9bc9c
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-10-01 18:31:27 -0400

    fix dataset naming

--- app/frontend/javascript/controllers/sortable_controller.js
+++ app/frontend/javascript/controllers/sortable_controller.js
@@ -1,6 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
 import { put } from "@rails/request.js";
 import Sortable from "sortablejs";
+
+/*
+ * Usage
+ * =====
+ *
+ * add data-controller="sortable" to common ancestor
+ *
+ * 1. Add data-sortable-url-value="URL to update position" to the
+ * controller container
+ *
+ * 2. Add data-sortable-handle to the drag handle:
+ *
+ * 3. Add data-sortable-id to the item
+ *
+ */
 export default class extends Controller {
   static values = { url: String };
 
@@ -17,8 +32,8 @@ export default class extends Controller {
 
   onEnd(event) {
     const { newIndex, item } = event;
-    const id = item.dataset["sortable-id"];
-    const url = this.urlValue.replace("ordering", id);
+    const id = item.dataset.sortableId;
+    const url = this.urlValue.replace(":id", id);
     put(url, {
       body: JSON.stringify({ ordering: newIndex }),
     });
