30ea6cd4a2b7578a8992c606083059410b26dfb3
maebeale <maebeale@users.noreply.github.com>
2025-10-05 22:41:45 +0000

    deploy: 825296b78b8352d6ad02104cebf5be724dc98d85

--- badges/main/app/controllers/dashboard_controller.rb
+++ badges/main/app/controllers/dashboard_controller.rb
@@ -1,22 +1,16 @@
 class DashboardController < ApplicationController
   skip_before_action :authenticate_user!, only: :help
 
-  layout "help", only: :help
-
   def index
-    @user = current_user.decorate
-    @workshops = current_user.curriculum(Workshop)
+    workshops = current_user.curriculum(Workshop)
       .featured.includes(:sectors).decorate
-    @workshops = @workshops.sort { |x, y| Date.parse(y.date) <=> Date.parse(x.date) }
+    @featured_workshops = workshops.sort { |x, y| Date.parse(y.date) <=> Date.parse(x.date) }
 
-    @resources = Resource.published.featured.where(kind: [nil, "Resource",
+    @popular_resources = Resource.published.featured.where(kind: [nil, "Resource",
       "Template", "Handout", "Scholarship", "Toolkit", "Form"])
       .decorate
 
     @stories = Resource.story.featured.decorate
-    @themes = Resource.theme.featured.decorate
-    @sector_impacts = Resource.sector_impact.featured.decorate
-    @recent_activity = current_user.recent_activity
   end
 
   def admin
