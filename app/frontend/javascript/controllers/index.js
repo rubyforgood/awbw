5c920c4995d20e7187d9e4e57c7244baa2723acf
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-10-06 18:05:37 -0400

    add stimulus controller for dimiss

--- app/frontend/javascript/controllers/index.js
+++ app/frontend/javascript/controllers/index.js
@@ -3,5 +3,8 @@ import { application } from "./application";
 import DropdownController from "./dropdown_controller";
 application.register("dropdown", DropdownController);
 
-import SortableController from "./sortable_controller"
-application.register("sortable", SortableController)
+import SortableController from "./sortable_controller";
+application.register("sortable", SortableController);
+
+import DismissController from "./dismiss_controller";
+application.register("dismiss", DismissController);
