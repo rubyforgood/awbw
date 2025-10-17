ede70fe263df33f6a9551acabb2f83abc1abf41d
maebeale <maebeale@users.noreply.github.com>
2025-10-16 22:39:37 -0400

    remove original style link tag

--- app/models/workshop_series_membership.rb
+++ app/models/workshop_series_membership.rb
@@ -3,4 +3,19 @@ class WorkshopSeriesMembership < ApplicationRecord
   belongs_to :workshop_child, class_name: "Workshop"
 
   validates :series_order, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def series_description_for(spanish: false, length: nil)
+    description =
+      if spanish
+        series_description_spanish.presence || series_description.presence
+      else
+        series_description.presence
+      end
+
+    if description.present?
+      length ? description.truncate(length) : description
+    else
+      workshop.formatted_objective(length: length)
+    end
+  end
 end
