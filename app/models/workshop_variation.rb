f54a069197fb59ef9deefc4c6c2d4847edd73fab
maebeale <maebeale@users.noreply.github.com>
2025-10-25 12:28:00 -0400

    Validate name and uniquness per workshop

--- app/models/workshop_variation.rb
+++ app/models/workshop_variation.rb
@@ -3,4 +3,5 @@ class WorkshopVariation < ApplicationRecord
 
   scope :active, -> { where(inactive: false) }
 
+  validates :name, presence: true, uniqueness: { scope: :workshop_id, case_sensitive: false }
 end
