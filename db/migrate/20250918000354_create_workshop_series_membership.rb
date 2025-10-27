770177f2679b5a1c93e6c00f2291f6aa78e48d7d
maebeale <maebeale@users.noreply.github.com>
2025-10-26 20:40:48 -0400

    Make migration reversible

--- db/migrate/20250918000354_create_workshop_series_membership.rb
+++ db/migrate/20250918000354_create_workshop_series_membership.rb
@@ -1,5 +1,5 @@
 class CreateWorkshopSeriesMembership < ActiveRecord::Migration[8.1]
-  def change
+  def up
     create_table :workshop_series_memberships do |t|
       t.integer :workshop_parent_id, null: false
       t.integer :workshop_child_id, null: false
@@ -8,7 +8,6 @@ class CreateWorkshopSeriesMembership < ActiveRecord::Migration[8.1]
       t.integer :series_order, default: 1, null: false
 
       t.timestamps
-
     end
 
     add_foreign_key :workshop_series_memberships, :workshops, column: :workshop_parent_id
@@ -16,5 +15,12 @@ class CreateWorkshopSeriesMembership < ActiveRecord::Migration[8.1]
 
     add_index :workshop_series_memberships, [:workshop_parent_id, :workshop_child_id], unique: true,
               name: "index_workshop_series_memberships_on_parent_and_child"
+  end
+
+  def down
+    remove_foreign_key :workshop_series_memberships, column: :workshop_parent_id
+    remove_foreign_key :workshop_series_memberships, column: :workshop_child_id
+    remove_index :workshop_series_memberships, name: "index_workshop_series_memberships_on_parent_and_child"
+    drop_table :workshop_series_memberships
   end
 end
