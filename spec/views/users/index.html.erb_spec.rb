7a3fc1cdbea1927de16df4b57e168f4daeb7274a
maebeale <maebeale@users.noreply.github.com>
2025-11-02 12:17:32 -0500

    Add permissions for tests to pass (these need to be removed)

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
+  let!(:combined_perm) { Permission.create!(security_cat: "Combined Adult and Children's Windows") }
+  let!(:adult_perm)    { Permission.create!(security_cat: "Adult Windows") }
+  let!(:children_perm) { Permission.create!(security_cat: "Children's Windows") }
+
   before(:each) do
     users = create_list(:user, 3, first_name: "Alice")
     paginated_users = WillPaginate::Collection.create(1, 10, users.size) do |pager|
