17d2b80468ccbd92f76f92d650d9745d7bf4fe30
maebeale <maebeale@users.noreply.github.com>
2025-11-07 22:58:17 -0500

    Update facilitator specs

--- spec/factories/facilitators.rb
+++ spec/factories/facilitators.rb
@@ -1,17 +1,8 @@
 FactoryBot.define do
   factory :facilitator do
+    association :user
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    primary_email_address { Faker::Internet.unique.email }
-    primary_email_address_type { 'Personal' }
-    street_address { Faker::Address.street_address }
-    city { Faker::Address.city }
-    state { Faker::Address.state }
-    zip { Faker::Address.postcode }
-    country { Faker::Address.country}
-    mailing_address_type { 'Personal' }
-    phone_number { Faker::PhoneNumber.phone_number }
-    phone_number_type { 'Personal' }
 
     trait :with_organization do
       after(:create) do |facilitator|
--- spec/models/facilitator_spec.rb
+++ spec/models/facilitator_spec.rb
@@ -1,27 +1,54 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Facilitator, type: :model do
-  describe 'associations' do
+  describe "associations" do
     it { should have_one(:user) }
     it { should have_many(:facilitator_organizations).dependent(:restrict_with_exception) }
     it { should have_many(:organizations).through(:facilitator_organizations) }
   end
 
-  describe 'validations' do
+  describe "validations" do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:primary_email_address) }
-    it { should validate_presence_of(:primary_email_address_type) }
-    it { should validate_presence_of(:street_address) }
-    it { should validate_presence_of(:city) }
-    it { should validate_presence_of(:state) }
-    it { should validate_presence_of(:zip) }
-    it { should validate_presence_of(:country) }
-    it { should validate_presence_of(:mailing_address_type) }
-    it { should validate_presence_of(:phone_number) }
-    it { should validate_presence_of(:phone_number_type) }
-    it { should validate_inclusion_of(:primary_email_address_type).in_array(%w[Work Personal]) }
-    it { should validate_inclusion_of(:mailing_address_type).in_array(%w[Work Personal]) }
-    it { should validate_inclusion_of(:phone_number_type).in_array(%w[Work Personal]) }
+  end
+
+  describe "#name" do
+    let(:user) { build(:user, first_name: "Jane", last_name: "Doe") }
+    let(:facilitator) { build(:facilitator, user: user) }
+
+    context "when display_name_preference is full_name" do
+      it "returns the full name" do
+        facilitator.display_name_preference = "full_name"
+        expect(facilitator.name).to eq("Jane Doe")
+      end
+    end
+
+    context "when display_name_preference is first_name_last_initial" do
+      it "returns first name and last initial" do
+        facilitator.display_name_preference = "first_name_last_initial"
+        expect(facilitator.name).to eq("Jane D")
+      end
+    end
+
+    context "when display_name_preference is first_name_only" do
+      it "returns only the first name" do
+        facilitator.display_name_preference = "first_name_only"
+        expect(facilitator.name).to eq("Jane")
+      end
+    end
+
+    context "when display_name_preference is last_name_only" do
+      it "returns only the last name" do
+        facilitator.display_name_preference = "last_name_only"
+        expect(facilitator.name).to eq("Doe")
+      end
+    end
+
+    context "when display_name_preference is nil or unknown" do
+      it "defaults to full name" do
+        facilitator.display_name_preference = nil
+        expect(facilitator.name).to eq("Jane Doe")
+      end
+    end
   end
 end
