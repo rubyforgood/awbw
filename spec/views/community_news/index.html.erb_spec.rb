bfbcc37dd13f3f7cbf305927386dd2a1d1360539
maebeale <maebeale@users.noreply.github.com>
2025-11-12 19:58:09 -0500

    Setup permissions in community news index spec
    Add permissions setup for community news index spec

--- spec/views/community_news/index.html.erb_spec.rb
+++ spec/views/community_news/index.html.erb_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "community_news/index", type: :view do
   before(:each) do
+    create(:permission, :adult)
+    create(:permission, :children)
+    create(:permission, :combined)
+    
     assign(:community_news, [
       CommunityNews.create!(
         title: "Title",
