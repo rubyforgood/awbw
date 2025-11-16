25eaa41bca8a0e026a2ca1a72a4ad0941fc50075
maebeale <maebeale@users.noreply.github.com>
2025-11-16 12:56:26 -0500

    Make all new buttons have similar styling and admin wrapper when needed (#392)
    * Make all new buttons have similar styling and admin wrapper when needed
    
    * Add user to users index spec

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "users/index", type: :view do
   let!(:combined_perm) { Permission.create!(security_cat: "Combined Adult and Children's Windows") }
   let!(:adult_perm)    { Permission.create!(security_cat: "Adult Windows") }
   let!(:children_perm) { Permission.create!(security_cat: "Children's Windows") }
+  let(:admin_user) { create(:user, :admin) } # or super_user trait
 
   before(:each) do
+    allow(view).to receive(:current_user).and_return(admin_user) # Stub current_user for Devise
     users = create_list(:user, 3, first_name: "Alice")
     paginated_users = WillPaginate::Collection.create(1, 10, users.size) do |pager|
       pager.replace(users)
