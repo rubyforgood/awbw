1a12438badf0e9f742f078b65cb2b2dc5ac0a672
maebeale <maebeale@users.noreply.github.com>
2025-11-16 18:57:06 -0500

    Add gsub to remove apostrophes to reduce flaky tests

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :project do
-    name { Faker::Company.name }
+    name { Faker::Company.name.gsub("'", " ") }
     association :windows_type
     association :location
     association :project_status
