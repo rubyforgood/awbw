eed3ca473a903e8d5a1ad436713c67f71261a41e
maebeale <maebeale@users.noreply.github.com>
2025-11-16 18:57:19 -0500

    Fix indentation of let statements

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "User login", type: :system do
-    let(:user) { create(:user) }
+  let(:user) { create(:user) }
 
   before do
     driven_by :selenium_chrome_headless
