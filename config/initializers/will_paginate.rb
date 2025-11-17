364a7d552857abf81c6d0722b2422e465cc57046
maebeale <maebeale@users.noreply.github.com>
2025-11-17 00:36:17 -0500

    Add custom TailwindPaginationRenderer

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,2 +1,7 @@
 require 'will_paginate/array'
 WillPaginate.per_page = 12
+
+Rails.application.config.to_prepare do
+	require Rails.root.join("app/renderers/tailwind_pagination_renderer.rb")
+	WillPaginate::ViewHelpers.pagination_options[:renderer] = TailwindPaginationRenderer
+end
