179a3e896852ea04c295011b98498df7e78e743f
maebeale <maebeale@users.noreply.github.com>
2025-11-17 03:10:06 +0000

    deploy: cafd541e22e018740d3c0266d2fd1246ac877618

--- badges/main/app/models/address.rb
+++ badges/main/app/models/address.rb
@@ -1,8 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
-  validates :street, presence: true
+  validates :locality, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :zip, presence: true
+
+  LOCALITIES = ["LA City", "LA County", "Southern CA", "Northern CA",
+                "Central CA", "Orange County", "Outside CA", "Outside USA"]
+
+  scope :active, -> { where(inactive: false) }
+
+  def name
+    "#{street}, #{city}, #{state} #{zip}"
+  end
 end
