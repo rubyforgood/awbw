94dc74e8c33a205a2ff108578fccd57db744a1f7
maebeale <maebeale@users.noreply.github.com>
2025-11-18 23:56:13 -0500

    Add bookmarks to all dashboard sections

--- app/models/community_news.rb
+++ app/models/community_news.rb
@@ -1,10 +1,11 @@
 class CommunityNews < ApplicationRecord
   belongs_to :project, optional: true
   belongs_to :windows_type, optional: true
-
   belongs_to :author, class_name: "User", optional: true
   belongs_to :created_by, class_name: "User"
   belongs_to :updated_by, class_name: "User"
+
+  has_many :bookmarks, as: :bookmarkable, dependent: :destroy
 
   validates :author_id, presence: true
   validates :title, presence: true
