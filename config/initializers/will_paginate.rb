cc8d15f21c148fdc8e64c4398f10a817da5dcd45
maebeale <maebeale@users.noreply.github.com>
2025-11-19 02:49:01 -0500

    Don't call pagination renderer in an initializer

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,7 +1,2 @@
 require 'will_paginate/array'
 WillPaginate.per_page = 12
-
-Rails.application.config.to_prepare do
-	require Rails.root.join("app/renderers/tailwind_pagination_renderer.rb")
-	WillPaginate::ViewHelpers.pagination_options[:renderer] = TailwindPaginationRenderer
-end
