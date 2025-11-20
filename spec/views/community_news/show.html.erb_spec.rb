e2ff48b43098ac6008ce6d0c59a9393850ff6a2d
maebeale <maebeale@users.noreply.github.com>
2025-11-19 19:54:21 -0500

    Update UX (#460)
    * Update My bookmarks page. Update news and story indexes esp using decorator
    
    * Fix tests

--- spec/views/community_news/show.html.erb_spec.rb
+++ spec/views/community_news/show.html.erb_spec.rb
@@ -27,8 +27,5 @@ RSpec.describe "community_news/show", type: :view do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/Youtube Url/)
-    expect(rendered).to match(/false/)
-    expect(rendered).to match(/Reference Url/)
   end
 end
