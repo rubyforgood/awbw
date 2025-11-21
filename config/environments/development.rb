c8b22ab9f84d0804ea9b48c96f5d86af0591d072
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2025-11-21 10:07:36 -0500

    use local storage for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :digitalocean
+  config.active_storage.service = :local
   Rails.application.routes.default_url_options[:host] ||= "http://localhost:3000"
 
   # Don't care if the mailer can't send.
