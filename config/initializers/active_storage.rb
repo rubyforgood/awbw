eb5fdd7e363edf5f063297c6efc13c4cd4643d53
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2025-11-22 14:09:10 -0500

    set app host for correct urls

--- config/initializers/active_storage.rb
+++
@@ -1,4 +0,0 @@
-ActiveSupport.on_load(:active_storage) do
-	host = Rails.application.routes.default_url_options[:host] || "http://localhost:3000"
-	ActiveStorage::Current.url_options = { host: host }
-end
