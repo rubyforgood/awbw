8a6b9e552b5c31650732fe0124293e097dba2c9d
maebeale <maebeale@users.noreply.github.com>
2025-11-23 23:19:52 -0500

    Set workshop main_image_url to use gallery image if no main_image

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -165,8 +165,10 @@ class Workshop < ApplicationRecord
   end
 
   def main_image_url
-    if images&.first&.file&.attached?
-      Rails.application.routes.url_helpers.url_for(images.first.file)
+    if main_image&.file&.attached?
+      Rails.application.routes.url_helpers.url_for(main_image.file)
+    elsif gallery_images.first&.file&.attached?
+      Rails.application.routes.url_helpers.url_for(gallery_images.first.file)
     else
       ActionController::Base.helpers.asset_path("workshop_default.jpg")
     end
