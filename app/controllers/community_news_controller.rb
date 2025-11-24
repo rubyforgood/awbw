21033b31248131dd5005530befd27f2fa8f25219
maebeale <maebeale@users.noreply.github.com>
2025-11-23 23:05:14 -0500

    Add new images partials. Add type to Image to make it STI. Filter CommunityNews based on user permissions

--- app/controllers/community_news_controller.rb
+++ app/controllers/community_news_controller.rb
@@ -3,7 +3,7 @@ class CommunityNewsController < ApplicationController
 
   def index
     per_page = params[:number_of_items_per_page].presence || 25
-    unpaginated = CommunityNews.all
+    unpaginated = current_user.super_user? ? CommunityNews.all : Community_news.published
     @community_news_count = unpaginated.count
     @community_news = unpaginated.paginate(page: params[:page], per_page: per_page)
   end
---
+++ app/models/images/square_image.rb
@@ -0,0 +1,2 @@
+class LogoImage < Image
+end
