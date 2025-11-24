f71358c51fbb5ec30d8379537eb67a759611516a
maebeale <maebeale@users.noreply.github.com>
2025-11-23 23:16:00 -0500

    Remove default scope on category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :metadatum
   has_many :categorizable_items, dependent: :destroy
   has_many :workshops, through: :categorizable_items, source: :categorizable, source_type: 'Workshop'
-  default_scope { order('name') }
+
   scope :published, -> { where(published: true) }
 
   # Validations
