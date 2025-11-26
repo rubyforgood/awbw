f7046395a82a72a64ede8336651013be6806acdf
Justin <16829344+jmilljr24@users.noreply.github.com>
2025-11-26 14:36:47 -0500

    add zip and word doc types (#499)
    * add zip and word doc types
    
    * use valid types

--- app/models/images/rich_text.rb
+++ app/models/images/rich_text.rb
@@ -1,6 +1,15 @@
 module Images
   class RichText < Image
-    ACCEPTED_CONTENT_TYPES = ["image/jpeg", "image/png", "image/gif", "application/pdf", "application/msword"].freeze
+    ACCEPTED_CONTENT_TYPES = [
+      "image/jpeg",
+      "image/png",
+      "image/gif",
+      "application/pdf",
+      "application/zip",
+      "application/msword", # Word .doc
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document", # Word .docx
+      "application/vnd.oasis.opendocument.text" # Word document .odt
+    ].freeze
 
     validates :file, content_type: ACCEPTED_CONTENT_TYPES
   end
