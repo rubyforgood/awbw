7b57f2bcb71b0a91ddc05e8eb363ca0264f23f34
jmilljr24 <jmilljr24@users.noreply.github.com>
2025-11-26 15:12:05 +0000

    deploy: e2cbd09d298417e115c6cd1c50589f305f6a8cc8

--- badges/main/app/models/images/gallery_image.rb
+++ badges/main/app/models/images/gallery_image.rb
@@ -1,4 +1,7 @@
 module Images
-	class GalleryImage < Image
-	end
+  class GalleryImage < Image
+    ACCEPTED_CONTENT_TYPES = ["image/jpeg", "image/png", "image/gif"].freeze
+
+    validates :file, content_type: ACCEPTED_CONTENT_TYPES
+  end
 end
--- badges/main/app/models/images/rich_text.rb
+++ badges/main/app/models/images/rich_text.rb
@@ -1,4 +1,7 @@
 module Images
   class RichText < Image
+    ACCEPTED_CONTENT_TYPES = ["image/jpeg", "image/png", "image/gif", "application/pdf", "application/msword"].freeze
+
+    validates :file, content_type: ACCEPTED_CONTENT_TYPES
   end
 end
