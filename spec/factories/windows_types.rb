31b8714bfc148c4690d4bd488079864faf3eae0b
maebeale <maebeale@users.noreply.github.com>
2025-11-27 01:29:31 -0500

    Update windows_type factory to have a short_name

--- spec/factories/windows_types.rb
+++ spec/factories/windows_types.rb
@@ -3,15 +3,18 @@ FactoryBot.define do
     sequence(:name) { |n| "Windows Type Name #{n}" }
 
     trait :adult do
-      name { 'ADULT WORKSHOP LOG' }
+      name { "ADULT WINDOWS" }
+      short_name { "Adult" }
     end
 
     trait :children do
-      name { 'CHILDREN WORKSHOP LOG'}
+      name { "CHILDREN'S WINDOWS" }
+      short_name { "Children's" }
     end
 
     trait :combined do
-      name { 'ADULT & CHILDREN COMBINED (FAMILY) LOG' }
+      name { "ADULT & CHILDREN COMBINED (FAMILY) WINDOWS" }
+      short_name { "Combined" }
     end
   end
 end 
