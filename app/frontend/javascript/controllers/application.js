1c987b0aae8f15bc4d31a6760e11728a4e2ebb79
maebeale <maebeale@users.noreply.github.com>
2025-12-02 09:55:15 -0500

    Change nav and mobile nav partials and items to use Facilitate/Community/Help instead of Workshops/Resources/FAQs

--- app/frontend/javascript/controllers/application.js
+++ app/frontend/javascript/controllers/application.js
@@ -1,10 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import TabsController from "./tabs_controller"
 
 const application = Application.start()
-
-// Register your controller
-application.register("tabs", TabsController)
 
 // Optional: enable debug mode and console access
 application.debug = true
--- app/frontend/javascript/controllers/index.js
+++ app/frontend/javascript/controllers/index.js
@@ -1,5 +1,14 @@
 import { application } from "./application"
 
+import CarouselController from "./carousel_controller"
+application.register("carousel", CarouselController)
+
+import CollectionController from "./collection_controller"
+application.register("collection", CollectionController)
+
+import DismissController from "./dismiss_controller";
+application.register("dismiss", DismissController);
+
 import DropdownController from "./dropdown_controller";
 application.register("dropdown", DropdownController);
 
@@ -9,20 +18,14 @@ application.register("file-preview", FilePreviewController)
 import SearchBoxController from "./search_box_controller"
 application.register("search-box", SearchBoxController)
 
-import SortableController from "./sortable_controller"
-application.register("sortable", SortableController)
-
-import TimeframeController from "./timeframe_controller"
-application.register("timeframe", TimeframeController)
-
-import DismissController from "./dismiss_controller";
-application.register("dismiss", DismissController);
-
-import CollectionController from "./collection_controller"
-application.register("collection", CollectionController)
-
 import ShareUrlController from "./share_url_controller"
 application.register("share-url", ShareUrlController)
 
-import CarouselController from "./carousel_controller"
-application.register("carousel", CarouselController)
+import SortableController from "./sortable_controller"
+application.register("sortable", SortableController)
+
+import TabsController from "./tabs_controller"
+application.register("tabs", TabsController)
+
+import TimeframeController from "./timeframe_controller"
+application.register("timeframe", TimeframeController)
