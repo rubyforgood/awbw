6e41b0f9ee838a3d9d06a25bd1617c0feaa6eee4
maebeale <maebeale@users.noreply.github.com>
2025-12-06 10:09:53 -0500

    Add inactive? to facilitator so it's bookmarkable

--- app/decorators/facilitator_decorator.rb
+++ app/decorators/facilitator_decorator.rb
@@ -9,25 +9,8 @@ class FacilitatorDecorator < Draper::Decorator
     "Facilitator Profile for #{first_name} #{last_name}"
   end
 
- DISPLAY_FIELDS = {
-    "First name" => :first_name,
-    "Last name" => :last_name,
-    "Primary email address" => :primary_email_address,
-    "Primary email type" => :primary_email_address_type,
-    "Street address" => :street_address,
-    "City" => :city,
-    "State" => :state,
-    "ZIP" => :zip,
-    "Country" => :country,
-    "Mailing address type" => :mailing_address_type,
-    "Phone number" => :phone_number,
-    "Phone number type" => :phone_number_type
-  }
-
-  def display_fields
-    DISPLAY_FIELDS.map do |label, method|
-      { label: label, value: object.send(method) }
-    end
+  def inactive?
+    !user ? false : user&.inactive?
   end
 
   def pronouns_display
