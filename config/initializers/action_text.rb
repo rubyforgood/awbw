4ae4811218ef1f98a4718c45e7698a4e86d2f611
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2025-12-18 06:17:37 -0500

    update allowed attributes

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -2,4 +2,4 @@ default_allowed_tags = Class.new.include(ActionText::ContentHelper).new.sanitize
 ActionText::ContentHelper.allowed_tags = default_allowed_tags.merge(%w[iframe table colgroup col tbody tr th td])
 
 default_allowed_attributes = Class.new.include(ActionText::ContentHelper).new.sanitizer_allowed_attributes
-ActionText::ContentHelper.allowed_attributes = default_allowed_attributes.merge(%w[style colspan rowspan width height align valign])
+ActionText::ContentHelper.allowed_attributes = default_allowed_attributes.merge(%w[style colspan rowspan cellpadding cellspacing width height align valign])
