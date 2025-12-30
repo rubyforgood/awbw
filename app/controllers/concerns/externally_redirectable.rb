1a915e7b7145a9356379d293e62b1feea1c95188
maebeale <maebeale@users.noreply.github.com>
2025-12-30 00:44:25 -0500

    Change redirect_to_external to use noopener noreferrer

--- app/controllers/concerns/externally_redirectable.rb
+++ app/controllers/concerns/externally_redirectable.rb
@@ -2,6 +2,6 @@ module ExternallyRedirectable
   extend ActiveSupport::Concern
 
   def redirect_to_external(url)
-    redirect_to url, allow_other_host: true
+    redirect_to url, allow_other_host: true, rel: "noopener noreferrer"
   end
 end
