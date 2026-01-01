8997fd5f023ece6ada64544f8edfc13b5d4e2476
diti <diti@diti.localdomain>
2026-01-01 20:58:01 +0200

    issue 46 / facilitator_submits_workshop_idea test

--- spec/system/facilitator_submits_workshop_idea_test.rb
+++ spec/system/facilitator_submits_workshop_idea_test.rb
@@ -12,8 +12,9 @@ RSpec.describe "Facilitators can submit a workshop idea", type: :system do
         user = create(:user)
         create(:facilitator, user: user)
         sign_in user
+
+      visit new_workshop_idea_path
         
-        visit new_workshop_idea_path
       end
       
       it "shows the new workshop form" do
