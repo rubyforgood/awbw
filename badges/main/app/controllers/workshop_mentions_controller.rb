2619f170694f00de300425fe7f925d4c8668cc99
jmilljr24 <jmilljr24@users.noreply.github.com>
2026-01-15 23:13:49 +0000

    deploy: 39783e2d757e7d43ba2f1958eac91cecbd95d8b4

--- badges/main/app/controllers/workshop_mentions_controller.rb
+++ badges/main/app/controllers/workshop_mentions_controller.rb
@@ -1,5 +1,6 @@
 class WorkshopMentionsController < ApplicationController
   def index
+    # TODO add action_policy scope for super_user
     @workshops = Workshop.where(id: params[:query])
     respond_to do |format|
       format.json
