94ac4387e3a376de2026bfe6815a12585ed2d7ea
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-01-15 18:00:44 -0500

    use default path for attachable

--- app/controllers/resource_mentions_controller.rb
+++ app/controllers/resource_mentions_controller.rb
@@ -1,5 +1,6 @@
 class ResourceMentionsController < ApplicationController
   def index
+    # TODO add action_policy scope for super_user
     @resources = Resource.where(id: params[:query])
     respond_to do |format|
       format.json
