e4c9d264f3835fe4ea30996339e31dbd732bdfdd
maebeale <maebeale@users.noreply.github.com>
2026-01-17 12:36:19 -0500

    Set default from addresses

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   include Rails.application.routes.url_helpers
+
   default from: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
   default reply_to: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
+
   layout "mailer"
 end
