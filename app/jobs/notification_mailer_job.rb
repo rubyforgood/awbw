4253b16ed99ed332fa96c432a0a321c136012f18
maebeale <maebeale@users.noreply.github.com>
2026-01-17 10:50:20 -0500

    Update NotificationMailerJob to focus on sending a mailer and optionally creating an associated notification

--- app/jobs/notification_mailer_job.rb
+++ app/jobs/notification_mailer_job.rb
@@ -1,31 +1,31 @@
 class NotificationMailerJob < ApplicationJob
   queue_as :default
 
-  def perform(notification_id)
+  def perform(notification_id, record_email_delivery: true)
     notification = Notification.find(notification_id)
 
-    case notification.kind
-    when "record_created"
-      mailer = NotificationMailer.created_notification(notification)
-    when "record_submitted"
-      mailer = NotificationMailer.submitted_notification(notification)
+    mailer_map = {
+      "idea_submitted_fyi"   => ->(n) { NotificationMailer.idea_submitted_fyi(n) },
+      "report_submitted_fyi" => ->(n) { NotificationMailer.report_submitted_fyi(n) },
+      "password_reset_fyi"   => ->(n) { NotificationMailer.password_reset_fyi(n.noticeable) }
+    }
+
+    mailer = mailer_map[notification.kind]&.call(notification)
+
+    unless mailer
+      raise "Unknown notification kind: #{notification.kind}"
     end
 
-    return if notification.delivered_at.present? # Idempotency guard
+    Notification.transaction do
+      notification.lock!
+      return if notification.delivered_at.present?
 
-    mailer.deliver_now
+      mailer.deliver_now
 
-    persist_email(notification, mailer)
+      NotificationServices::PersistDeliveredEmail.call(
+        notification: notification,
+        mail: mailer
+      ) if record_email_delivery
+    end
   end
-end
-
-private
-
-def persist_email(notification, mail)
-  notification.update!(
-    email_subject: mail.subject,
-    email_body_html: mail.html_part&.body&.decoded,
-    email_body_text: mail.text_part&.body&.decoded,
-    delivered_at: Time.current
-  )
 end
