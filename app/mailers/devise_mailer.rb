9fab1057858654282a316c67701c4f9334140302
maebeale <maebeale@users.noreply.github.com>
2026-01-17 10:51:20 -0500

    Update DeviseMailer to create notifications for reset password, account confirmed, and account locked, but only send admin 'fyi' email/notification for password reset

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,29 +1,33 @@
 class DeviseMailer < Devise::Mailer
   helper ApplicationHelper
-<<<<<<< HEAD
   include Rails.application.routes.url_helpers
   before_action :set_branding
+  after_action :create_notification_record
 
   default from: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
   default reply_to: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
 
+  def reset_password_instructions(record, token, opts = {})
+    @record = record
+    @token  = token
+    @mail   = super
+  end
+
+  def confirmation_instructions(record, token, opts = {})
+    @record = record
+    @token  = token
+    @mail   = super
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    @record = record
+    @token  = token
+    @mail   = super
+  end
+
   def default_url_options
     Rails.application.config.action_mailer.default_url_options
   end
-=======
-
-  before_action :set_branding
-
-  default from: ENV.fetch(
-    "REPLY_TO_EMAIL",
-    "no-reply@awbw.org"
-  )
-
-  default reply_to: ENV.fetch(
-    "REPLY_TO_EMAIL",
-    "programs@awbw.org"
-  )
->>>>>>> 5dfd1ed1 (Add local DeviseMailer class to override defaults and add explicit from and reply_to, and pass @organization_name to views)
 
   protected
 
@@ -38,5 +42,52 @@ class DeviseMailer < Devise::Mailer
       organization_name: @organization_name
     )
     headers
+  end
+
+  private
+
+  def notification_kind_for_devise_action
+    {
+      "reset_password_instructions" => "password_reset",
+      "confirmation_instructions"   => "account_confirmation",
+      "unlock_instructions"         => "account_unlock"
+    }
+  end
+
+  def create_notification_record
+    return unless @mail && @record
+
+    kind = notification_kind_for_devise_action[action_name]
+    return unless kind # don't create fyi emails for Devise mailers you don’t care about
+
+    notification = NotificationServices::CreateNotification.call(
+      noticeable: @record,
+      recipient_role: "facilitator",
+      recipient_email: @record.email,
+      kind: kind,
+      notification_type: 1,
+      deliver: false # Devise already sent the email, so no need to deliver via the job
+    )
+
+    NotificationServices::PersistDeliveredEmail.call(
+      notification: notification,
+      mail: @mail # record the Devise email that was just sent
+    )
+
+    notify_admin_if_needed(kind)
+  end
+
+
+  def notify_admin_if_needed(kind)
+    return unless kind == "password_reset"
+
+    NotificationServices::CreateNotification.call(
+      noticeable: @record,
+      recipient_role: "admin",
+      recipient_email: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org"),
+      kind: "password_reset_fyi",
+      notification_type: 1,
+      deliver: true
+    )
   end
 end
