82330e477ad32b20667003d6f2d122be88456f60
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-01-17 13:07:08 -0500

    add optional owner to assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,7 @@
 class Asset < ApplicationRecord
   self.inheritance_column = :type
 
-  belongs_to :owner, polymorphic: true
+  belongs_to :owner, polymorphic: true, optional: true
   belongs_to :report, optional: true
 
   has_one_attached :file, dependent: :purge do |attachable|
