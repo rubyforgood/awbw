a841a6e0b22c7b0a8183f3e44b2a90fba5e19803
maebeale <maebeale@users.noreply.github.com>
2026-01-17 11:36:13 -0500

    Add reset_password_fyi to mailer previews

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -1,4 +1,16 @@
 class NotificationMailerPreview < ActionMailer::Preview
+  def reset_password_fyi
+    notification =
+      Notification.create!(
+        noticeable: User.first,
+        notification_type: 1,
+        kind: "reset_password_fyi",
+        recipient_role: "admin",
+        recipient_email: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
+      )
+    NotificationMailer.reset_password_fyi(notification)
+  end
+
   def idea_submitted_fyi
     noticeable = StoryIdea.first || WorkshopIdea.first
     notification =
