e8b3caa4903fd281ba9010baaf02d0bbd48f266e
maebeale <maebeale@users.noreply.github.com>
2026-01-17 10:56:46 -0500

    Update CreateNotification to require notification_type and to pass record_email_delivery boolean to NotificationMailerJob (bc in the case of devise mailers, we don't want to record_email_delivery bc we do it in the DeviseMailer where we have access to the sent mail

--- app/services/notification_services/create_notification.rb
+++ app/services/notification_services/create_notification.rb
@@ -5,8 +5,9 @@ module NotificationServices
       recipient_role:,
       recipient_email:,
       kind:,
-      notification_type: :created_record,
-      deliver: true
+      notification_type:,
+      deliver: true,
+      record_email_delivery: true
     )
       notification = Notification.create!(
         noticeable: noticeable,
@@ -16,7 +17,7 @@ module NotificationServices
         recipient_email: recipient_email
       )
 
-      NotificationMailerJob.perform_later(notification.id) if deliver
+      NotificationMailerJob.perform_later(notification.id, record_email_delivery: record_email_delivery) if deliver
 
       notification
     end
