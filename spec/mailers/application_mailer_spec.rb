5126bf5fc6a3227c102f26551ff03b5fdf8c710e
maebeale <maebeale@users.noreply.github.com>
2026-01-18 10:50:52 -0500

    Replace default from emails w ENV fetch

--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicationMailer do
   it 'sets the default from address' do
-    expect(described_class.default[:from]).to eq('noreply@awbw.org')
+    expect(described_class.default[:from]).to eq(ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org"))
   end
 
   it 'uses the correct layout' do
