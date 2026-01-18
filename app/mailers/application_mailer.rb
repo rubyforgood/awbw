626e00cc13314e02769f0a1e27d913811ed5a0fa
maebeale <maebeale@users.noreply.github.com>
2026-01-18 10:50:14 -0500

    Change default mailer settings for default from and reply_to. Add def default_url_options and include Rails.application.routes.url_helpers to devise mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@awbw.org"
+  include Rails.application.routes.url_helpers
+  default from: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
+  default reply_to: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
   layout "mailer"
 end
--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,17 +1,14 @@
 class DeviseMailer < Devise::Mailer
   helper ApplicationHelper
-
+  include Rails.application.routes.url_helpers
   before_action :set_branding
 
-  default from: ENV.fetch(
-    "REPLY_TO_EMAIL",
-    "no-reply@awbw.org"
-  )
+  default from: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
+  default reply_to: ENV.fetch("REPLY_TO_EMAIL", "programs@awbw.org")
 
-  default reply_to: ENV.fetch(
-    "REPLY_TO_EMAIL",
-    "programs@awbw.org"
-  )
+  def default_url_options
+    Rails.application.config.action_mailer.default_url_options
+  end
 
   protected
 
