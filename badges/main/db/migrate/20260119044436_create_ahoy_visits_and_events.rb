880f05927edb48048ca9b546962099322c6abbb0
maebeale <maebeale@users.noreply.github.com>
2026-01-19 05:10:13 +0000

    deploy: edb89e5d72bc19ba052d90b793f6fc365fc3585d

--- badges/main/db/migrate/20260119044436_create_ahoy_visits_and_events.rb
+++ badges/main/db/migrate/20260119044436_create_ahoy_visits_and_events.rb
@@ -45,7 +45,7 @@ class CreateAhoyVisitsAndEvents < ActiveRecord::Migration[8.1]
     end
 
     add_index :ahoy_visits, :visit_token, unique: true
-    add_index :ahoy_visits, [:visitor_token, :started_at]
+    add_index :ahoy_visits, [ :visitor_token, :started_at ]
 
     create_table :ahoy_events do |t|
       t.references :visit
