3a4726793265727ddfcd04246118cd2da514d439
maebeale <maebeale@users.noreply.github.com>
2026-01-19 14:29:00 -0500

    Make migration reversible

--- db/migrate/20260119060000_remove_counter_cache_columns_from_models.rb
+++ db/migrate/20260119060000_remove_counter_cache_columns_from_models.rb
@@ -1,42 +1,47 @@
 class RemoveCounterCacheColumnsFromModels < ActiveRecord::Migration[8.1]
-  def change
-    # Remove view_count columns and indexes
-    remove_index :community_news, name: "index_community_news_on_view_count"
-    remove_column :community_news, :view_count, :integer, default: 0, null: false
+  COUNTER_CACHES = {
+    community_news: { view_count: { index: "index_community_news_on_view_count" } },
+    events: { view_count: { index: "index_events_on_view_count" } },
+    facilitators: { view_count: { index: "index_facilitators_on_view_count" } },
+    projects: { view_count: { index: "index_projects_on_view_count" } },
+    quotes: { view_count: { index: "index_quotes_on_view_count" } },
+    stories: { view_count: { index: "index_stories_on_view_count" } },
+    tutorials: { view_count: { index: "index_tutorials_on_view_count" } },
+    workshop_variations: { view_count: { index: "index_workshop_variations_on_view_count" } },
+    resources: {
+      view_count:     { index: "index_resources_on_view_count" },
+      print_count:    { index: "index_resources_on_print_count" },
+      download_count: { index: "index_resources_on_download_count" } },
+    workshops: {
+      view_count:  { index: "index_workshops_on_view_count" },
+      print_count: { index: "index_workshops_on_print_count" } }
+  }.freeze
 
-    remove_index :events, name: "index_events_on_view_count"
-    remove_column :events, :view_count, :integer, default: 0, null: false
+  def up
+    COUNTER_CACHES.each do |table, columns|
+      columns.each do |column, config|
+        if config[:index] && index_exists?(table, name: config[:index])
+          remove_index table, name: config[:index]
+        end
 
-    remove_index :facilitators, name: "index_facilitators_on_view_count"
-    remove_column :facilitators, :view_count, :integer, default: 0, null: false
+        if column_exists?(table, column)
+          remove_column table, column
+        end
+      end
+    end
+  end
 
-    remove_index :projects, name: "index_projects_on_view_count"
-    remove_column :projects, :view_count, :integer, default: 0, null: false
+  def down
+    COUNTER_CACHES.each do |table, columns|
+      columns.each do |column, config|
+        unless column_exists?(table, column)
+          add_column table, column, :integer, default: 0, null: false
+        end
 
-    remove_index :quotes, name: "index_quotes_on_view_count"
-    remove_column :quotes, :view_count, :integer, default: 0, null: false
-
-    remove_index :stories, name: "index_stories_on_view_count"
-    remove_column :stories, :view_count, :integer, default: 0, null: false
-
-    remove_index :tutorials, name: "index_tutorials_on_view_count"
-    remove_column :tutorials, :view_count, :integer, default: 0, null: false
-
-    remove_index :workshop_variations, name: "index_workshop_variations_on_view_count"
-    remove_column :workshop_variations, :view_count, :integer, default: 0, null: false
-
-    # Remove view_count, print_count, download_count from resources
-    remove_index :resources, name: "index_resources_on_view_count"
-    remove_index :resources, name: "index_resources_on_print_count"
-    remove_index :resources, name: "index_resources_on_download_count"
-    remove_column :resources, :view_count, :integer, default: 0, null: false
-    remove_column :resources, :print_count, :integer, default: 0, null: false
-    remove_column :resources, :download_count, :integer, default: 0, null: false
-
-    # Remove view_count and print_count from workshops
-    remove_index :workshops, name: "index_workshops_on_view_count"
-    remove_index :workshops, name: "index_workshops_on_print_count"
-    remove_column :workshops, :view_count, :integer, default: 0, null: false
-    remove_column :workshops, :print_count, :integer, default: 0, null: false
+        if config[:index] && !index_exists?(table, name: config[:index])
+          add_index table, column, name: config[:index]
+        end
+      end
+    end
   end
 end
