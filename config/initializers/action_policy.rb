0a4dfd2b979718b968d87683ce5416bc84f9dde6
Edwin Mak <edwinthinks@gmail.com>
2026-01-25 11:17:16 -0500

    Fix ActionPolicy initializer - remove invalid configure block
    ActionPolicy doesn't use ActionPolicy.configure block syntax.
    Configuration is done via class attributes instead.

--- config/initializers/action_policy.rb
+++ config/initializers/action_policy.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-ActionPolicy.configure do |config|
-  # Require authorization on every controller action
-  # Enable after verifying all controllers have proper authorization
-  # config.fail_if_not_authorized = true
-end
+# ActionPolicy configuration
+# See https://actionpolicy.evilmartians.io/#/configuration for options
+
+# Require authorization on every controller action
+# Enable after verifying all controllers have proper authorization
+# ActionPolicy::Base.authorize_targets = true
