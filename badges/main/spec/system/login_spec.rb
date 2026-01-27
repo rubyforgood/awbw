eda6e34eb81c620b22ad7b4f1d88534d2297cddb
jmilljr24 <jmilljr24@users.noreply.github.com>
2026-01-27 21:00:20 +0000

    deploy: 01d10acd9d81ef7a8c639a09a52d9e4f74acc406

--- badges/main/spec/system/login_spec.rb
+++ badges/main/spec/system/login_spec.rb
@@ -1,26 +1,26 @@
-require "rails_helper"
-
-RSpec.describe "User login", type: :system do
-  let(:user) { create(:user) }
-
-  it "shows default avatar when logged out" do
-    visit root_path
-    expect(page).to_not have_css("#avatar")
-    expect(page).to_not have_css("img[src*='missing.png']")
-  end
-
-  scenario "User login shows avatar only after login" do
-    visit root_path
-
-    # Logged out state
-    expect(page).not_to have_css("#avatar")
-
-    # Log in
-    sign_in user
-    visit root_path
-
-    # Logged in state
-    expect(page).to have_css("#avatar")
-    expect(page).to have_css("#avatar-image")
-  end
-end
+# require "rails_helper"
+#
+# RSpec.describe "User login", type: :system do
+#   let(:user) { create(:user) }
+#
+#   it "shows default avatar when logged out" do
+#     visit root_path
+#     expect(page).to_not have_css("#avatar")
+#     expect(page).to_not have_css("img[src*='missing.png']")
+#   end
+#
+#   scenario "User login shows avatar only after login" do
+#     visit root_path
+#
+#     # Logged out state
+#     expect(page).not_to have_css("#avatar")
+#
+#     # Log in
+#     sign_in user
+#     visit root_path
+#
+#     # Logged in state
+#     expect(page).to have_css("#avatar")
+#     expect(page).to have_css("#avatar-image")
+#   end
+# end
