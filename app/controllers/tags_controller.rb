e60d5c7cca6cb8412ad2c70b5116bc9091ccc419
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-02-03 10:27:20 -0500

    remove unnecessary joins

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,29 +3,16 @@ class TagsController < ApplicationController
   end
 
   def sectors
-    @sectors = Rails.cache.fetch("published_sectors_with_sectorable_items", expires_in: 1.month) do
-      Sector
-        .includes(:sectorable_items)
-        .references(:sectorable_items)
-        .published
-        .distinct
-        .order(:name)
-        .to_a
-    end
+    @sectors = Sector.published.order(:name)
   end
 
   def categories
-    @categories_by_type = Rails.cache.fetch("published_categories_by_type", expires_in: 1.month) do
-      categories =
-        Category
-          .includes(:category_type, :categorizable_items)
-          .references(:category_type, :categorizable_items)
-          .published
-          .select("categories.*, category_types.name AS category_type_name")
-          .distinct
-          .order(Arel.sql("category_type_name, categories.position, categories.name"))
-
-      categories.group_by(&:category_type_name)
-    end
+    @categories_by_type = Category
+      .published
+      .joins(:category_type)
+      .select("categories.*, category_types.name AS category_type_name")
+      .order("category_types.name, categories.position, categories.name")
+      .to_a
+      .group_by(&:category_type_name)
   end
 end
