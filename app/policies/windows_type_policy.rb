51ab32a0404d9d4151393a902deec52bd57abbd0
maebeale <maebeale@users.noreply.github.com>
2026-02-04 14:42:18 -0500

    Add windows type policy

---
+++ app/policies/windows_type_policy.rb
@@ -0,0 +1,5 @@
+class WindowsTypePolicy < ApplicationPolicy
+  # See https://actionpolicy.evilmartians.io/#/writing_policies
+  #
+  # override or add new rules here that are not defined in ApplicationPolicy
+end
