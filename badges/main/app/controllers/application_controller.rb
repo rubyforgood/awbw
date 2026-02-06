440f9595280125ebea638293be108bfdea90f30b
jmilljr24 <jmilljr24@users.noreply.github.com>
2026-02-06 23:29:51 +0000

    deploy: 79180cf5d21de931dd651c0d6c07d7854dee74de

--- badges/main/app/controllers/application_controller.rb
+++ badges/main/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!  # ensures only logged-in users can access pages
   before_action :set_current_user # for AhoyTrackable in models
   after_action :flush_lifecycle_events
+  around_action :set_time_zone_from_user, if: :current_user
 
   # TODO add this callback to verify
   # that `authorize!` has been called in all controllers
@@ -30,6 +31,15 @@ class ApplicationController < ActionController::Base
       new_user_password_path
     else
       root_path
+    end
+  end
+
+  def set_time_zone_from_user
+    zone = ActiveSupport::TimeZone[current_user&.time_zone]
+    if zone
+      Time.use_zone(zone) { yield }
+    else
+      yield
     end
   end
 
