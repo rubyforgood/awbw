3e82094fdebc8c5583e5310ebee62953bd1a46e0
Justin <16829344+jmilljr24@users.noreply.github.com>
2026-02-08 10:36:14 -0500

    use event_regiatrion policy which matches the record (#864)

--- app/controllers/events/registrations_controller.rb
+++ app/controllers/events/registrations_controller.rb
@@ -4,7 +4,7 @@ module Events
 
     def create
       @event_registration = @event.event_registrations.new(registrant: current_user)
-      authorize! :event_registration, to: :create?
+      authorize! @event_registration
 
       if @event_registration.save
         success = "You have successfully registered for this event."
@@ -20,51 +20,32 @@ module Events
         end
       end
     end
-    #
-    # def destroy
-    #   registration = @event.event_registrations.find_by(registrant: current_user)
-    #   authorize! :event_registration, to: :destroy?
-    #
-    #   unless registration
-    #     return respond_with_alert("Registration not found")
-    #   end
-    #
-    #   if registration.destroy
-    #     respond_with_notice("You are no longer registered.")
-    #   else
-    #     respond_with_alert(registration.errors.full_messages.to_sentence)
-    #   end
-    # end
 
     def destroy
-      registration = @event.event_registrations.find_by(registrant: current_user)
-      authorize! :event_registration, to: :destroy?
+      @event_registration = @event.event_registrations.find_by(registrant: current_user)
+      authorize! @event_registration
 
-      unless registration
+      unless @event_registration
+        alert = "Registration not found"
         respond_to do |format|
-          format.turbo_stream do
-            flash.now[:alert] = "Registration not found"
-            render turbo_stream: turbo_stream.replace(
-              "flash",
-              partial: "shared/flash_messages"
-            )
-          end
-          format.html { redirect_to @event, alert: "Registration not found" }
+          format.turbo_stream { flash.now[:alert] = alert }
+          format.html { redirect_to @event, alert: alert }
         end
         return
       end
 
-      registration.destroy!
-
-      respond_to do |format|
-        format.turbo_stream do
-          flash.now[:notice] = "You are no longer registered."
-          render turbo_stream: turbo_stream.replace(
-            "flash",
-            partial: "shared/flash_messages"
-          )
+      if @event_registration.destroy
+        success = "You are no longer registered."
+        respond_to do |format|
+          format.turbo_stream { flash.now[:notice] = success }
+          format.html { redirect_to @event, notice: success }
         end
-        format.html { redirect_to @event, notice: "You are no longer registered." }
+      else
+        error = @event_registration.errors.full_messages.to_sentence
+        respond_to do |format|
+          format.turbo_stream { flash.now[:alert] = error }
+          format.html { redirect_to @event, alert: error }
+        end
       end
     end
 
