e5c09288bf7637b858e454e744314c89eb9ab094
maebeale <maebeale@users.noreply.github.com>
2026-02-08 09:24:31 -0500

    Cleanup to get specs running again

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Awbw
   class Application < Rails::Application
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.paths.add "app/extras", eager_load: true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
@@ -23,14 +31,6 @@ module Awbw
       Rails.root.join("spec/mailers/previews"),
       Rails.root.join("lib/mailer_previews")
     ]
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g| # scaffold generator settings
       # g.orm :active_record             # Use ActiveRecord (default)
