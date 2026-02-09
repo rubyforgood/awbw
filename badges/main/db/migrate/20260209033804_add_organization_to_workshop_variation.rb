c6c7951774c307801d972e094a58864e4c24a7ed
jmilljr24 <jmilljr24@users.noreply.github.com>
2026-02-09 13:37:25 +0000

    deploy: aa8e638053c06e28e3204cfcd2d68d5001a60b18

--- badges/main/db/migrate/20260209033804_add_organization_to_workshop_variation.rb
+++ badges/main/db/migrate/20260209033804_add_organization_to_workshop_variation.rb
@@ -1,15 +1,9 @@
 class AddOrganizationToWorkshopVariation < ActiveRecord::Migration[8.1]
   def up
-    change_column :workshop_variations, :organization_id, :integer
-
-    unless foreign_key_exists?(:workshop_variations, :organizations)
-      add_foreign_key :workshop_variations, :organizations
-    end
+    # no-op: this migration was broken and replaced
   end
 
   def down
-    remove_foreign_key :workshop_variations, :organizations if foreign_key_exists?(:workshop_variations, :organizations)
-
-    change_column :workshop_variations, :organization_id, :bigint
+    # no-op
   end
 end
