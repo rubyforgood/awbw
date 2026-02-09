8af387902d129bf96a494a8aae947d6c6a8875ca
maebeale <maebeale@users.noreply.github.com>
2026-02-08 23:50:31 -0500

    Update CRUD admin-only styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_09_005954) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_09_033804) do
   create_table "action_text_mentions", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "action_text_rich_text_id", null: false
     t.datetime "created_at", null: false
