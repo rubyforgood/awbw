1e519c4b8ca3dfcd118cf2de6a2be1136b6efb32
Justin <16829344+jmilljr24@users.noreply.github.com>
2026-02-09 14:25:53 -0500

    Solid cache (#892)
    * add solid-cache
    
    gem
    config
    
    * cache migration
    
    * update config
    
    * cache workshop results
    
    * update database urls for cache
    
    * fix cache names on row and card
    
    * formatting
    
    * revert
    
    revert
    
    * fix config
    
    comment out dev cache
    
    * schema

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
+  # config.cache_store = :solid_cache_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # Active Storage
