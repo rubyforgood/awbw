2f7f22b9e60cb0a08d9ee0285c54453a79b4d353
maebeale <maebeale@users.noreply.github.com>
2026-02-09 03:00:17 -0500

    Make notifications index searchable (#882)
    * Make notifications index searchable
    
    * rubocop

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -32,6 +32,22 @@ class Notification < ApplicationRecord
   validates :recipient_email, presence: true
   validates :notification_type, presence: true
 
+  # Scopes
+  scope :email, ->(email) { where("notifications.recipient_email LIKE ?", "%#{email}%") }
+  scope :participant_name, ->(name) { joins(:people)
+      .joins("INNER JOIN users ON users.email = notifications.recipient_email")
+      .where("people.name LIKE ?", "%#{name}%") }
+  scope :record_type, ->(record_type) { where(noticeable_type: record_type.to_s.camelize.titleize.gsub(" ", "")) }
+  scope :subject_line, ->(subject) { where("notifications.email_subject LIKE ?", "%#{subject}%") }
+
+  def self.search_by_params(params)
+    stories = is_a?(ActiveRecord::Relation) ? self : all
+    stories = stories.email(params[:email]) if params[:email].present?
+    stories = stories.participant_name(params[:participant_name]) if params[:participant_name].present?
+    stories = stories.subject_line(params[:subject_line]) if params[:subject_line].present?
+    stories
+  end
+
   def delivered?
     delivered_at.present?
   end
