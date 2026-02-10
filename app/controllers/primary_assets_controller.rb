f8b83791cc6d637cec67237d4ffc92123ef552e6
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-02-10 08:58:22 -0500

    add show page image upload

---
+++ app/controllers/primary_assets_controller.rb
@@ -0,0 +1,31 @@
+ class PrimaryAssetsController < ApplicationController
+   include ActionView::RecordIdentifier
+   before_action :set_owner, only: [ :create ]
+
+   def create
+     authorize! :library_asset, to: :create?
+     ActiveRecord::Base.transaction do
+       if existing_primary = @owner.assets.find_by(type: "PrimaryAsset")
+         existing_primary.update!(type: "GalleryAsset")
+       end
+
+       @asset = @owner.assets.build(asset_params.except(:file))
+       @asset.type = "PrimaryAsset"
+       @asset.file.attach(asset_params[:file]) if asset_params[:file].present?
+       @asset.save!
+     end
+
+     redirect_back_or_to polymorphic_path(@owner)
+   end
+
+
+   private
+
+   def set_owner
+     @owner = GlobalID::Locator.locate_signed(params[:owner_sgid]) if params[:owner_sgid]
+   end
+
+   def asset_params
+     params.expect(primary_asset: [ :file, :sgid ])
+   end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   #   resources :gallery_assets, only: [ :show ]
   # end
   resources :library_assets
+  resources :primary_assets
   resources :rich_text_assets
 
   namespace :images do
