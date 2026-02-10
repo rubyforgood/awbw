545b2cc9dc9f391a7d91b0df0b951c562fc1b14f
maebeale <maebeale@users.noreply.github.com>
2026-02-10 07:16:44 -0500

    Fix test failures

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Notification do
   describe 'associations' do
-    it { should belong_to(:noticeable) }
+    it { should belong_to(:noticeable).optional }
   end
 end
