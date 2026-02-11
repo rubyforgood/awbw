fa685f7dd8338bdda113ec0338712452c646c633
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-02-11 07:45:02 -0500

    query in controller

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -199,6 +199,7 @@ class WorkshopsController < ApplicationController
     @leader_spotlights = @workshop.associated_resources.leader_spotlights.where(published: true)
     @workshop_variations = @workshop.workshop_variations.published
     @sectors = @workshop.sectorable_items.map { |item| item.sector if item.sector.published? }.compact if @workshop.sectorable_items.any?
+    @mentions = @workshop.all_mentions_grouped
   end
 
 
