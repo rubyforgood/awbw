93fbb5927b891cc5d6168162c1777bcc7a228b7e
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-02-11 16:49:27 -0500

    try this

--- config/initializers/mini-profiler.rb
+++ config/initializers/mini-profiler.rb
@@ -1,11 +1,15 @@
 if defined?(Rack::MiniProfiler)
-  # Enable only in non-production environments
+  # Enable in development and staging, disable in production
   if Rails.env.production?
     Rack::MiniProfiler.config.enabled = false
+  elsif Rails.env.staging?
+    Rack::MiniProfiler.config.enabled = true
+    Rack::MiniProfiler.config.position = "bottom-left"
+    Rack::MiniProfiler.config.enable_hotwire_turbo_drive_support = true
   else
     Rack::MiniProfiler.config.enabled = true
     Rack::MiniProfiler.config.position = "bottom-left"
-
     Rack::MiniProfiler.config.enable_hotwire_turbo_drive_support = true
   end
+end
 end
