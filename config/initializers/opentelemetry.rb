439449e51d202c564153c983f1d09385baa7c2d8
John Paul Ashenfelter <john@ashenfelter.com>
2026-02-15 18:38:05 -0500

    Simplify otel using env vars

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,20 +1,9 @@
-if Rails.env.production?
+unless Rails.env.test?
   require "opentelemetry/sdk"
   require "opentelemetry/exporter/otlp"
   require "opentelemetry/instrumentation/all"
 
   OpenTelemetry::SDK.configure do |c|
-    c.service_name = ENV.fetch("OTEL_SERVICE_NAME", "awbw")
-
-    c.add_span_processor(
-      OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-        OpenTelemetry::Exporter::OTLP::Exporter.new(
-          endpoint: "https://api.honeycomb.io",
-          headers: { "x-honeycomb-team" => ENV.fetch("HONEYCOMB_API_KEY") }
-        )
-      )
-    )
-
-    c.use_all
+    c.use_all()
   end
 end
