b49c5cdb5f8ea1c7e0df0c761ac453ace8f2aad3
johnpaulashenfelter <johnpaulashenfelter@users.noreply.github.com>
2026-02-16 01:32:25 +0000

    deploy: a30dc0b7d5c62c6a32e55c64f19b1282bbd09812

---
+++ badges/main/config/initializers/opentelemetry.rb
@@ -0,0 +1,9 @@
+unless Rails.env.local?
+  require "opentelemetry/sdk"
+  require "opentelemetry/exporter/otlp"
+  require "opentelemetry/instrumentation/all"
+
+  OpenTelemetry::SDK.configure do |c|
+    c.use_all()
+  end
+end
