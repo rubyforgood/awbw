4ec2497281dba09baa7b59917e85a7ff20e3e195
maebeale <maebeale@users.noreply.github.com>
2026-02-15 21:43:24 -0500

    Add if not exists to safeguard migration

--- db/migrate/20260215122713_add_polymorphic_index_to_bookmarks.rb
+++ db/migrate/20260215122713_add_polymorphic_index_to_bookmarks.rb
@@ -1,6 +1,7 @@
 class AddPolymorphicIndexToBookmarks < ActiveRecord::Migration[7.2]
   def change
     add_index :bookmarks, [ :bookmarkable_type, :bookmarkable_id ],
-              name: "index_bookmarks_on_bookmarkable"
+              name: "index_bookmarks_on_bookmarkable",
+              if_not_exists: true
   end
 end
