a486858e1ebd0e7af20dd3037e921506b239cd82
maebeale <maebeale@users.noreply.github.com>
2026-02-17 08:21:27 -0500

    Update report/monthly_report/workshop_log policy and scopes

--- app/policies/monthly_report_policy.rb
+++ app/policies/monthly_report_policy.rb
@@ -3,6 +3,14 @@ class MonthlyReportPolicy < ApplicationPolicy
     authenticated?
   end
 
+  def new?
+    authenticated?
+  end
+
+  def create?
+    authenticated?
+  end
+
   def show?
     admin? || owner? || member?
   end
@@ -15,18 +23,22 @@ class MonthlyReportPolicy < ApplicationPolicy
     record.persisted? && (admin? || owner? || member?)
   end
 
+  relation_scope do |relation|
+    next relation if admin?
+    scope = relation.where(user_id: user.id) # owned logs
+    if user.person&.organization_ids.present?
+      scope = scope.or(relation.organization_ids(user.person&.organization_ids)) # logs from person's affiliations
+    end
+    scope
+  end
+
   private
 
   def member?
     @member ||= begin
-      return false unless authenticated?
-      return false unless record.organization
-      record.organization.affiliations.exists?(person_id: user.person_id)
-    end
-  end
-
-  relation_scope do |relation|
-    next relation if admin?
-    relation.where(user_id: user.id)
+                  return false unless authenticated?
+                  return false unless record.organization
+                  record.organization.affiliations.exists?(person_id: user.person_id)
+                end
   end
 end
