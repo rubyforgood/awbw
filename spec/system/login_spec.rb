a4cd24596a1ec80e75ded875a9af703f3f899f5e
maebeale <maebeale@users.noreply.github.com>
2026-02-17 15:12:10 -0500

    Add system spec to validate login isn't broken

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe "User login", type: :system do
   let(:password) { "MyString" }
+  let(:generic_error) { "Invalid email or password. Please contact us for assistance." }
 
   def fill_in_login(email, password)
     visit new_user_session_path
@@ -19,34 +20,50 @@ RSpec.describe "User login", type: :system do
       fill_in_login(user.email, password)
 
       expect(page).to have_current_path(new_user_session_path)
-      expect(page).to have_content("Your account is locked")
+      expect(page).to have_content("Please contact us for assistance.")
     end
   end
 
   context "when user is inactive" do
     let(:user) { create(:user, password: password, inactive: true) }
 
-    it "does not allow login and shows inactive message" do
+    it "does not allow login and shows generic error" do
       fill_in_login(user.email, password)
 
       expect(page).to have_current_path(new_user_session_path)
-      expect(page).to have_content("inactive")
+      expect(page).to have_content(generic_error)
     end
   end
 
   context "when user exceeds maximum failed login attempts" do
     let(:user) { create(:user, password: password) }
+    let(:last_attempt_warning) { "You have one more attempt before your account is locked" }
 
-    it "locks the account after 10 failed attempts and shows locked message" do
-      10.times do
+    it "warns on the last attempt then locks the account" do
+      9.times do
         fill_in_login(user.email, "wrong_password")
       end
+
+      expect(page).to have_content(last_attempt_warning)
+
+      fill_in_login(user.email, "wrong_password")
 
       fill_in_login(user.email, password)
 
       expect(page).to have_current_path(new_user_session_path)
-      expect(page).to have_content("Your account is locked")
+      expect(page).to have_content("Please contact us for assistance.")
       expect(user.reload.locked_at).to be_present
+    end
+  end
+
+  context "when credentials are wrong" do
+    let(:user) { create(:user, password: password) }
+
+    it "shows generic error" do
+      fill_in_login(user.email, "wrong_password")
+
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_content(generic_error)
     end
   end
 
@@ -56,8 +73,7 @@ RSpec.describe "User login", type: :system do
     it "allows login successfully" do
       fill_in_login(user.email, password)
 
-      expect(page).not_to have_content("Your account is locked")
-      expect(page).not_to have_content("inactive")
+      expect(page).not_to have_content(generic_error)
       expect(page).to have_css("#avatar")
     end
   end
