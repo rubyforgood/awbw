c0e08c45fa0324417b2f85c9bbdaac6ee01c0a07
maebeale <maebeale@users.noreply.github.com>
2026-02-17 14:58:30 -0500

    Login testing spec

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,26 +1,64 @@
-# require "rails_helper"
-#
-# RSpec.describe "User login", type: :system do
-#   let(:user) { create(:user) }
-#
-#   it "shows default avatar when logged out" do
-#     visit root_path
-#     expect(page).to_not have_css("#avatar")
-#     expect(page).to_not have_css("img[src*='missing.png']")
-#   end
-#
-#   scenario "User login shows avatar only after login" do
-#     visit root_path
-#
-#     # Logged out state
-#     expect(page).not_to have_css("#avatar")
-#
-#     # Log in
-#     sign_in user
-#     visit root_path
-#
-#     # Logged in state
-#     expect(page).to have_css("#avatar")
-#     expect(page).to have_css("#avatar-image")
-#   end
-# end
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "User login", type: :system do
+  let(:password) { "MyString" }
+
+  def fill_in_login(email, password)
+    visit new_user_session_path
+    fill_in "user_email", with: email
+    fill_in "user_password", with: password
+    click_button "Log in"
+  end
+
+  context "when user is locked" do
+    let(:user) { create(:user, :locked, password: password) }
+
+    it "does not allow login and shows locked message" do
+      fill_in_login(user.email, password)
+
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_content("Your account is locked")
+    end
+  end
+
+  context "when user is inactive" do
+    let(:user) { create(:user, password: password, inactive: true) }
+
+    it "does not allow login and shows inactive message" do
+      fill_in_login(user.email, password)
+
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_content("inactive")
+    end
+  end
+
+  context "when user exceeds maximum failed login attempts" do
+    let(:user) { create(:user, password: password) }
+
+    it "locks the account after 10 failed attempts and shows locked message" do
+      10.times do
+        fill_in_login(user.email, "wrong_password")
+      end
+
+      fill_in_login(user.email, password)
+
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_content("Your account is locked")
+      expect(user.reload.locked_at).to be_present
+    end
+  end
+
+  context "when user is active and unlocked" do
+    let(:user) { create(:user, password: password) }
+
+    it "allows login successfully" do
+      fill_in_login(user.email, password)
+
+      expect(page).not_to have_content("Your account is locked")
+      expect(page).not_to have_content("inactive")
+      expect(page).to have_css("#avatar")
+    end
+  end
+end
