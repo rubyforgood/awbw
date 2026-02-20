a6456c99a30b31c52037dcc3456bd3c3a9213b4e
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-02-20 18:31:47 -0500

    add search to org form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -122,19 +122,6 @@ class OrganizationsController < ApplicationController
     @organization_statuses = OrganizationStatus.all
     @sectors_collection = Sector.published.order(:name).pluck(:name, :id)
     @current_sector_ids = @organization.sectorable_items.map(&:sector_id)
-    # Build array of [display_name, id] for person selection dropdown
-    # Email priority matches Person#preferred_email: user.email > person.email > person.email_2
-    @people_array = authorized_scope(Person.left_joins(:user))
-                          .order(:first_name, :last_name)
-                          .pluck(
-                            :first_name,
-                            :last_name,
-                            :id,
-                            Arel.sql("COALESCE(users.email, people.email, people.email_2)")
-                          )
-                          .map { |fn, ln, id, email|
-                            [ "#{fn} #{ln}#{" (#{email})" if email.present?}", id ]
-                          }
 
     if @organization.persisted? && @organization.errors.empty?
       affiliations = @organization.affiliations
