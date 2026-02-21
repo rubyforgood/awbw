f93264f2838b4a45b7345d4d2194cc42c29560eb
Ryan Vasios <rvasios@wrapbook.com>
2026-02-20 23:14:39 -0500

    Update tags_controller to use scopings on sectors and categories (#1080)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,13 +7,14 @@ class TagsController < ApplicationController
 
   def sectors
     authorize! Sector, to: :tags_index?
-    @sectors = Sector.published.order(:name)
+    @sectors = Sector.published.has_taggings.order(:name)
   end
 
   def categories
     authorize! Category, to: :tags_index?
     @categories_by_type = Category
       .published
+      .has_taggings
       .joins(:category_type)
       .select("categories.*, category_types.name AS category_type_name")
       .order("category_types.name, categories.position, categories.name")
