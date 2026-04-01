b3d0158f2bacd6c3877b0b688ad10c190929fc66
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-04-01 16:37:01 -0400

    add video_recordings to the spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Home", type: :request do
   end
 
   describe "GET /home/* (turbo frame lazy loading)" do
-    %w[workshops resources events community_news stories].each do |section|
+    %w[workshops resources events community_news stories video_recordings].each do |section|
       it "#{section} returns 200 for visitors" do
         get "/home/#{section}"
         expect(response).to have_http_status(:ok)
