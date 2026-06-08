f910026bb9147d559950b00277054d4bb913166b
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-06-08 19:56:11 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_08_205816) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_08_184843) do
   create_table "action_text_mentions", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "action_text_rich_text_id", null: false
     t.datetime "created_at", null: false
@@ -470,7 +470,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_08_205816) do
     t.boolean "autoshow_title", default: true, null: false
     t.boolean "autoshow_videoconference_label", default: true, null: false
     t.boolean "autoshow_videoconference_link", default: true, null: false
-    t.boolean "bulk_payment_enabled", default: false, null: false
     t.integer "cost_cents"
     t.datetime "created_at", null: false
     t.integer "created_by_id"
