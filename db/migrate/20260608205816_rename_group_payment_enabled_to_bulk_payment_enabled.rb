5dfab2e43e0775566bd9fcea4bbd15a80ff7cafd
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-06-08 19:51:33 -0400

    clean up

--- db/migrate/20260608205816_rename_group_payment_enabled_to_bulk_payment_enabled.rb
+++
@@ -1,5 +0,0 @@
-class RenameGroupPaymentEnabledToBulkPaymentEnabled < ActiveRecord::Migration[8.1]
-  def change
-    rename_column :events, :group_payment_enabled, :bulk_payment_enabled
-  end
-end
