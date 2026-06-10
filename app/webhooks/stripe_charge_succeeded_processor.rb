8e345476905255c5112e7817a9097c55920df589
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-06-10 12:33:03 -0400

    disable email

--- app/webhooks/stripe_charge_succeeded_processor.rb
+++ app/webhooks/stripe_charge_succeeded_processor.rb
@@ -6,28 +6,33 @@ class StripeChargeSucceededProcessor
     return unless stripe_charge.paid
 
     metadata = stripe_charge.metadata || {}
-
     return if (metadata.keys & APP_METADATA_KEYS).any?
-    return if Pay::Charge.exists?(processor_id: stripe_charge.id)
     return if ExternalProcessorPayment.with_metadata_key("stripe_charge_id", stripe_charge.id).exists?
 
+    pay_charge = Pay::Charge.find_by(processor_id: stripe_charge.id)
+    return if pay_charge && ExternalProcessorPayment.exists?(pay_charge_id: pay_charge.id)
+    create_payment(stripe_charge, pay_charge: pay_charge)
+  end
+
+  private
+
+  def create_payment(stripe_charge, pay_charge: nil)
     payment = ExternalProcessorPayment.new(
-      person: anonymous_payer,
+      person: external_payer,
       amount_cents: stripe_charge.amount,
       amount_cents_remaining: stripe_charge.amount,
       currency: stripe_charge.currency,
+      pay_charge_id: pay_charge&.id,
       metadata: {
         stripe_charge_id: stripe_charge.id,
         stripe_charge: stripe_charge.to_hash
       }
     )
-    payment.skip_pay_charge_validation = true
+    payment.skip_pay_charge_validation = true if pay_charge.nil?
     payment.save!
   end
 
-  private
-
-  def anonymous_payer
+  def external_payer
     Person.find_or_create_by!(first_name: "External", last_name: "Payer")
   end
 end
