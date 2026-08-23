b2cae728f4c00cd034642b3ddf09029165944b87
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-08-22 20:30:08 -0400

    turbo frame pagination section

--- app/renderers/fm_archive_pagination_renderer.rb
+++ app/renderers/fm_archive_pagination_renderer.rb
@@ -4,8 +4,21 @@ class FmArchivePaginationRenderer < TailwindPaginationRenderer
   def url(page)
     @template.url_for(
       @template.params.to_unsafe_h
-               .merge(@options[:param_name] => page)
-               .except(:controller, :action, :page)
+               .except(:controller, :action)
+               .except(*page_params)
+               .merge(param_name => page)
     )
+  end
+
+  private
+
+  def param_name
+    @options[:param_name] || :page
+  end
+
+  # Sibling sections' page params must not ride along in a section's links,
+  # or paging one section would freeze every other section at its old page.
+  def page_params
+    @template.params.keys.select { |key| key.to_s.end_with?("page") }
   end
 end
