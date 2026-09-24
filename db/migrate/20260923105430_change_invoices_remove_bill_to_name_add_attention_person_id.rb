fc13f3327608401937eec82a34ddbcf72a806d7c
Justin Miller <16829344+jmilljr24@users.noreply.github.com>
2026-09-24 09:54:01 -0400

    creat single clean migration file

--- db/migrate/20260923105430_change_invoices_remove_bill_to_name_add_attention_person_id.rb
+++
@@ -1,7 +0,0 @@
-class ChangeInvoicesRemoveBillToNameAddAttentionPersonId < ActiveRecord::Migration[8.1]
-  def change
-    remove_column :invoices, :bill_to_name, :string
-    remove_column :invoices, :attention, :string
-    add_reference :invoices, :attention_person, foreign_key: { to_table: :people }
-  end
-end
--- db/migrate/20260923105534_remove_attention_string_from_invoices.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAttentionStringFromInvoices < ActiveRecord::Migration[8.1]
-  def change
-    remove_column :invoices, :attention, :string
-  end
-end
